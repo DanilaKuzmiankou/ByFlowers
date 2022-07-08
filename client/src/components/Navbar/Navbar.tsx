4eff3230d74fa9ae3106409b3a14e5391b052480
Danila Kuzmiankou Notebook <stmail468@gmail.com>
2022-07-08 11:33:36 +0300

    Changed Navbar Buttons style

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -15,8 +15,30 @@ import AdbIcon from '@mui/icons-material/Adb';
 import './Navbar.css'
 import flowerEmblem from '../../assets/images/flowersEmblem.png';
 
-const pages = ['Blog', 'Categories', 'About us'];
+const siteLogo = 'FlowersDelivery'
+const pages = ['Blog', 'About us', 'Categories'];
+const pages2 = ['Blog', 'About us'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+const navbarButtonsStyle = {
+    my: 2,
+    mx:0.5,
+    fontFamily: 'DefaultFonts',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '999px',
+    backgroundColor: 'hsla(0, 0%, 100%, 0.06)',
+    transition: 'background-color 200ms ease',
+    color: '#fff',
+    fontSize: '1.10rem',
+    lineHeight: 1,
+    fontWeight: 700,
+    paddingTop: '14px',
+    paddingBottom: '14px',
+    paddingRight: '13px',
+    paddingLeft: '13px'
+}
 
 const ResponsiveAppBar = () => {
     const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
@@ -69,7 +91,7 @@ const ResponsiveAppBar = () => {
                             textDecoration: 'none'
                         }}
                     >
-                        FlowersDelivery{'\n'}
+                        {siteLogo}
                     </Typography>
                     <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, justifyContent:'flex-end'}}>
                         <IconButton
@@ -107,7 +129,13 @@ const ResponsiveAppBar = () => {
                             ))}
                         </Menu>
                     </Box>
-                    <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                    <Box
+                        component="img"
+                        sx={{ display: { xs: 'flex', md: 'none' }, mr: 1, height: 55,
+                            width: 75}}
+                        alt="The house from the offer."
+                        src={require("../../assets/images/flowersEmblem3.png")}
+                    />
                     <Typography
                         variant="h5"
                         noWrap
@@ -123,34 +151,35 @@ const ResponsiveAppBar = () => {
                             textDecoration: 'none',
                         }}
                     >
-                        LOGO
+                        {siteLogo}
                     </Typography>
+
+                    <Box/>
                     <Box  sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'end' }}>
-                        {pages.map((page) => (
+                        {pages2.map((page) => (
                             <Button
                                 key={page}
                                 onClick={handleCloseNavMenu}
-                                sx={{
-                                    my: 2,
-                                    mx:0.5,
-                                    fontFamily: 'DefaultFonts',
-                                    display: 'flex',
-                                    justifyContent: 'center',
-                                    alignItems: 'center',
-                                    borderRadius: '999px',
-                                    backgroundColor: 'hsla(0, 0%, 100%, 0.06)',
-                                    transition: 'background-color 200ms ease',
-                                    color: '#fff',
-                                    fontSize: '1.25rem',
-                                    lineHeight: 1,
-                                    fontWeight: 700,
-                                    paddingTop: '14px',
-                                    paddingBottom: '14px',
-                            }}
+                                sx={navbarButtonsStyle}
                             >
                                 {page}
                             </Button>
                         ))}
+                        <Button
+                            key='Categories'
+                            onClick={handleCloseNavMenu}
+                            sx={navbarButtonsStyle}
+                        >
+                            Categories
+                            <div className="nav_dropdown-arrow w-embed">
+                                <svg width="14" height="6" viewBox="0 0 14 6" fill="none"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M12.0063 0.310204C12.348 0.0606614 12.902 0.0606614 13.2437 0.310204C13.5854 0.559746 13.5854 0.964334 13.2437 1.21388L7.61872 5.32169C7.27701 5.57123 6.72299 5.57123 6.38128 5.32169L0.756282 1.21388C0.414573 0.964334 0.414573 0.559746 0.756282 0.310204C1.09799 0.0606614 1.65201 0.0606614 1.99372 0.310204L7 3.96618L12.0063 0.310204Z"
+                                        fill="currentColor"></path>
+                                </svg>
+                            </div>
+                        </Button>
                     </Box>
 
                     <Box sx={{ flexGrow: 0, pl: '10px' }}>
