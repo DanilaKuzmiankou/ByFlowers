2a709f8ba2327e5347635b2b00df762249e6ed64
Danila Kuzmiankou Notebook <stmail468@gmail.com>
2022-07-08 12:35:50 +0300

    Added new pages template (Blog, BestChoice, AboutUs, NotFound pages)

--- client/src/pages/AboutUs/AboutUs.tsx
+++ client/src/pages/AboutUs/AboutUs.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const MainPage = () => {
+export const AboutUs = () => {
     return (
         <div>
             
         </div>
     );
 };
-
-export default MainPage;
