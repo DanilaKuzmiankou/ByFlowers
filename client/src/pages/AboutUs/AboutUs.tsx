88298c6f7716bbaa851607937a50d30380f17a6a
Danila Kuzmiankou Notebook <stmail468@gmail.com>
2022-07-09 13:41:32 +0300

    AboutUs.tsx added main picture

--- client/src/pages/AboutUs/AboutUs.tsx
+++ client/src/pages/AboutUs/AboutUs.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Box from "@mui/material/Box";
 
 export const AboutUs = () => {
     return (
         <div>
-            About Us
+            <Box
+                component="img"
+                sx={{
+                    height: '100%',
+                    width: '100%'
+            }}
+                alt="The house from the offer."
+                src={require("../../assets/images/mainPicture.jpg")}
+            />
+
         </div>
     );
 };
