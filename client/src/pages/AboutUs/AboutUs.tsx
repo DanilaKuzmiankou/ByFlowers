edd42f5c09b39377f98d3c03499d96fd2ffbea9a
St4ffer <stmail468@gmail.com>
2022-07-13 17:30:12 +0300

    Added carousel with 4 pictures, started work with comment section

--- client/src/pages/AboutUs/AboutUs.tsx
+++ client/src/pages/AboutUs/AboutUs.tsx
@@ -1,19 +1,110 @@
 import React from 'react';
 import Box from "@mui/material/Box";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
+import './AboutUs.css'
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Autoplay, EffectFade, Navigation, Pagination, Parallax} from "swiper";
 
 export const AboutUs = () => {
-    return (
-        <div>
-            <Box
-                component="img"
-                sx={{
-                    height: '100%',
-                    width: '100%'
-            }}
-                alt="The house from the offer."
-                src={require("../../assets/images/mainPicture.jpg")}
-            />
 
-        </div>
+    return (
+        <>
+            <Swiper
+                speed={600}
+                spaceBetween={0}
+                effect='fade'
+                slidesPerView={1}
+                navigation={true}
+                loop={true}
+                pagination={{
+                    clickable: true,
+                }}
+                autoplay={{
+                    delay: 3500,
+                    disableOnInteraction: false,
+                }}
+                modules={[EffectFade, Autoplay, Navigation, Pagination]}
+            >
+
+                <SwiperSlide>
+                    <div
+                        className="parallax-bg"
+                        style={{
+                            backgroundImage: 'url(' + require("../../assets/images/carouselFlowers.jpg") + ')'
+                        }}
+                    >
+                    <div className="text" >
+                        <p>
+                            Best shop ever
+                        </p>
+                    </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div
+                        className="parallax-bg"
+                        style={{
+                            backgroundImage: 'url(' + require("../../assets/images/carouselFlowers2.jpg") + ')'
+                        }}
+                    >
+                        <div className="text" >
+                            <p>
+                                Cool text
+                            </p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div
+                        className="parallax-bg"
+                        style={{
+                            backgroundImage: 'url(' + require("../../assets/images/carouselFlowers3.jpg") + ')'
+                        }}
+                    >
+                        <div className="text" >
+                            <p>
+                                Another interesting text
+                            </p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div
+                        className="parallax-bg"
+                        style={{
+                            backgroundImage: 'url(' + require("../../assets/images/carouselFlowers4.jpg") + ')'
+                        }}
+                    >
+                        <div className="text" >
+                            <p>
+                                And another one
+                            </p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+            <div className='comments'>
+                <div className='comment1'>
+                    <p>
+                        And another one
+                    </p>
+                    <p>
+                        And another one
+                    </p>
+                </div>
+                <div className='comment2'>
+                    <p>
+                        And another one
+                    </p>
+                    <p>
+                        And another one
+                    </p>
+                </div>
+            </div>
+        </>
+
     );
 };
