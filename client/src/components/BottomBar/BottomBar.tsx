51935f19de03b9df797acc8a53f33b8783417838
St4ffer <stmail468@gmail.com>
2022-07-18 13:22:26 +0300

    RE-moved bottom bar to new component

---
+++ client/src/components/BottomBar/BottomBar.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Box from "@mui/material/Box";
+import "./BottomBar.css"
+
+export const BottomBar = () => {
+    return (
+        <div className='bottom-bar'>
+            <div className='bottom-trademarks'>
+                <div className='bottom-brand'>
+                    FlowersMarket
+                </div>
+                <div>
+                    &copy; All rights reserved
+                </div>
+                <div>
+                    2020-2022
+                </div>
+            </div>
+            <div className='bottom-icons'>
+                <Box
+                    component="img"
+                    sx={{ height: 45,
+                        width: 45, marginRight: '7px'}}
+                    src={require("../../assets/images/instagramIcon.png")}
+                />
+                <Box
+                    component="img"
+                    sx={{ height: 45,
+                        width: 45}}
+                    src={require("../../assets/images/facebookIcon.png")}
+                />
+            </div>
+        </div>
+    );
+};
