0758687791425a39e46a86c23fc011e711405aca
St4ffer <stmail468@gmail.com>
2022-07-19 15:21:15 +0300

    Server was created. Models were created.

---
+++ index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const sequelize = require("./DB");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+//app.use("/api", router);
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        console.log('Started!')
+        app.listen(PORT);
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+start();
