ec36575b7146281ae6bc1c21dc75d556544620e6
Danila Kuzmiankou Notebook <stmail468@gmail.com>
2022-08-11 14:11:58 +0300

    Changes: added: ProductService.js (Splited ProductController.js logic) updated: getFlowersTypes and getPlantsTypes ProductService.js methods was merged into one getProductsTypes(bool isFlower)

---
+++ service/ProductService.js
@@ -0,0 +1,121 @@
+const {ProductType, ProductPicture, Product} = require("../models/Models");
+const ApiError = require("../error/ApiError");
+const {Op, Sequelize} = require("sequelize");
+
+
+let productService = this;
+
+class ProductService {
+
+    constructor() {
+        productService = this;
+    }
+
+    async createProduct(name, description, productType, count, price, pictures, isFlower) {
+        let createdProduct
+        let productTypeDb
+        try {
+            createdProduct = await Product.create({name, description, count, price, isFlower});
+        } catch (e) {
+            throw ApiError.badRequest("This product is already created!");
+        }
+        productTypeDb = await ProductType.findOne({where: {name: productType}})
+        if (!productTypeDb) productTypeDb = await ProductType.create({name: productType})
+        const picturesDB = [];
+        for (let picture of pictures) {
+            if (picture) {
+                let dbPicture = await ProductPicture.create({picture})
+                picturesDB.push(dbPicture)
+            }
+        }
+        await createdProduct.setPictures(picturesDB);
+        await createdProduct.setProductType(productTypeDb)
+        return createdProduct
+    }
+
+
+
+    async getProductsWithType(types, minPrice, maxPrice, limit, offset) {
+        const whereExpression = {
+            price: {
+                [Op.gte]: minPrice !== -1 ? minPrice : 0,
+                [Op.lte]: maxPrice !== -1 ? maxPrice : Number.MAX_VALUE
+            }
+        }
+        const includeExpression = [
+            {
+                model: ProductType,
+                as: 'productType',
+                attributes: [],
+                where: {name: types},
+
+            },
+            {
+                model: ProductPicture,
+                as: 'pictures',
+                attributes: ['picture']
+            }
+        ]
+        let filterExpression = ''
+        for (let type of types) {
+            if (types[0] === type) filterExpression += `name='${type}'`
+            filterExpression += `, "productType".name='${type}'`
+        }
+        const orderExpression =
+            [
+                [{model: ProductType, as: 'productType'}, Sequelize.literal(filterExpression)],
+                ['updatedAt', 'ASC']
+            ]
+        const count = await Product.count({
+            where: whereExpression,
+            include: includeExpression
+        });
+        const products = await productController.getProducts(whereExpression, includeExpression, orderExpression, limit, offset)
+        return {products, count}
+    }
+
+
+    async getRecommendationProducts(limit) {
+        return await productService.getProducts(
+            {},
+            [
+                {
+                    model: ProductPicture,
+                    as: 'pictures',
+                    attributes: ['picture']
+                }
+            ],
+            Sequelize.literal('random()'),
+            Number(limit),
+            0
+        )
+    }
+
+
+    getProducts(whereExpression, includeExpression, orderExpression, limit, offset) {
+        return Product.findAll({
+            where: whereExpression,
+            limit: limit,
+            offset: offset,
+            subQuery: false,
+            include: includeExpression,
+            order: orderExpression
+        })
+    }
+
+    async getProductsTypes(isFlower) {
+        return await ProductType.findAll({
+            attributes: ['name'],
+            include: [
+                {
+                    model: Product,
+                    as: "product",
+                    attributes: [],
+                    where: {isFlower}
+                }
+            ]
+        });
+    }
+}
+
+module.exports = new ProductService()
