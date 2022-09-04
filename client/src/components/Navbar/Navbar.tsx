d83fd5f439ac40087ca14a32a4f7682c6563b27a
St4ffer <stmail468@gmail.com>
2022-09-04 20:42:00 +0300

    deploy fixes

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -286,6 +286,24 @@ export const Navbar = observer(() => {
                       {page}
                     </Button>
                   ))}
+                  <CustomHoverMenu
+                    menuName="Plants"
+                    buttonStyle={navbarButtonsStyle}
+                    menuItemsNames={productsStore.plants}
+                    onMenuItemClick={(plantName: string) =>
+                      switchPage('products', plantName, false)
+                    }
+                    isFlowers={false}
+                  />
+                  <CustomHoverMenu
+                    menuName="Flowers"
+                    buttonStyle={navbarButtonsStyle}
+                    menuItemsNames={productsStore.flowers}
+                    onMenuItemClick={(flowerName: string) =>
+                      switchPage('products', flowerName, true)
+                    }
+                    isFlowers
+                  />
                 </Box>
                 <div
                   style={{
--- index.js
+++ index.js
@@ -14,7 +14,13 @@ const app = express()
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
-
+    app.get('/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'), function(error) {
+            if (error) {
+                res.status(500).send(error)
+            }
+        })
+    })
 }
 const corsOptions = {
     origin: process.env.CLIENT_URL,
