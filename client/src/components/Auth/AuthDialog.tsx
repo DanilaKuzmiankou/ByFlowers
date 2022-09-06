fb080bac7a8f6962ce31790e5c9ae9ceaec6f494
St4ffer <stmail468@gmail.com>
2022-09-06 21:34:19 +0300

    Login and Sign in components moved from separate pages into auth dialog

---
+++ client/src/components/Auth/AuthDialog.tsx
@@ -0,0 +1,37 @@
+import { Dialog, DialogContent } from '@mui/material'
+import { observer } from 'mobx-react-lite'
+import Box from '@mui/material/Box'
+import { Login } from './Login/Login'
+import userStore from '../../store/UserStore'
+import { Signin } from './Signin/SignIn'
+import { CloseButton } from '../CloseButton/CloseButton'
+
+export const AuthDialog = observer(() => {
+  const handleClose = () => {
+    userStore.setIsAuthDialogOpen(false)
+  }
+
+  return (
+    <Dialog
+      fullWidth={false}
+      maxWidth="xs"
+      keepMounted
+      open={userStore.isAuthDialogOpen}
+      onClose={handleClose}
+    >
+      <DialogContent
+        sx={{
+          backgroundColor: '#d2e4fd',
+          padding: '10px 16px',
+          position: 'relative',
+          overflow: 'clip',
+        }}
+      >
+        <Box sx={{ position: 'absolute', top: '-3px', right: '-3px' }}>
+          <CloseButton closeFunction={handleClose} />
+        </Box>
+        {userStore.isLoginPageOpen ? <Login /> : <Signin />}
+      </DialogContent>
+    </Dialog>
+  )
+})
